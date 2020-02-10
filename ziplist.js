805536596942703b4aa9455931a954d3ddcac048
kvndngyn <nk279@hawaii.edu>
2020-02-10 10:05:38 -1000

    update ziplist.js

--- ziplist.js
+++ ziplist.js
@@ -4,8 +4,7 @@ const testNumber = [1, 2, 3];
 function zipList(letters, numbers) {
   const zip = [];
   for (let i = 0; i < letters.length; i++) {
-    zip.push(letters[i]);
-    zip.push(numbers[i]);
+    zip.push(letters[i], numbers[i]);
   }
   return zip;
 }
